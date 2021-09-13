68671da3d3a36fe9442e288a11d5b135198daa2e
jlaw <jlaw@warren.k12.in.us>
2021-09-13 07:50:27 -0400

    Initial commit

---
+++ src/SalesCommission.java
@@ -0,0 +1,27 @@
+/*
+A car salesperson typically earns an hourly wage and a commission on any sales they completed. They typically work 30 to 40
+hours a week, earning $5.25 to $7.50 an hour. Additionally, they earn 1% to 3% of all sales they've completed that week,
+which a typical car sells for $10,000 to $30,000. A salesperson with a 1% commission that sells a car for $20,000 earns $200.00.
+
+Write a program which prompts the user to enter the hourly rate, hours worked, the sales commission percent and the total
+sales of a salesperson and calculates their total earnings. The program should include multiple methods. (What ARE the
+smaller pieces?)
+
+Example:
+
+Hourly Rate: 6.25
+Hours Worked: 33.5
+Sales Commission: 1.5%
+Total Sales: $65,000.00
+
+Total Earnings: $1184.38
+
+ */
+
+public class SalesCommission {
+
+    public static void main(String[] args) {
+
+    }
+
+}
